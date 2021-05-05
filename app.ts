0d859a997c35d39a9cd525c55b87a79b893302a4
Chayanit McCormick <chayanit.chaisri@gmail.com>
2021-05-04 21:07:44 -0400

    added examples typescript

---
+++ app.ts
@@ -0,0 +1,49 @@
+// string
+// number 
+// boolean
+
+// array
+// object
+
+// all letters are lowercase
+// first type that we set it will be that type
+
+// Literl type
+const myName = "Chay"
+
+// number type
+let age = 26;
+age = "25"; // it will warn about type
+
+// string type
+let favColor: string;
+favColor = "pink"
+
+// obj type
+type Person = {
+    name: string,
+    age: number,
+    hobbies: string[],
+    favNumbers: Array<number> // same with number []
+}
+
+const person: Person = {
+    name: "Chay",
+    age: 20,
+    hobbies: ['cooking', 'eating'],
+    favNumbers: [1,2,3,4,5]
+}
+
+
+// set type for number
+// num2? - means optional
+const add = (num1: number, num2:number) => {
+    return num1 + num2
+}
+
+const result = add(1, 4);
+
+console.log(result) // 5
+
+// union type
+type Name = string | number;
